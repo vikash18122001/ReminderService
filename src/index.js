5c1b153ce3572cb31bc758c3ab794cd86caa3933
vikash maurya <vikashghazipur2001@gmail.com>
2023-08-08 22:13:49 +0530

    basic setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/server-config');
+
+
+const setupAndStartServer = async () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`);
+       
+        
+    });
+}
+
+setupAndStartServer();
